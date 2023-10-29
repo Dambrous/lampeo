0bca0b1b6b1b81206ddbb6ad62f0d813f2b4bdbf
Luca D'Ambrosio <luca2001dambro@gmail.com>
2023-10-29 11:05:20 +0100

    [VERSION 1.1] correct total price of order in (my_orders) page

--- front-end/user/pages/my_orders.php
+++ front-end/user/pages/my_orders.php
@@ -4,7 +4,6 @@ $om = new OrderManager();
 $oim = new OrderItemManager();
 $pm = new ProductManager();
 $orders = $om->get_orders($logged_in_user->id);
-$total = 0;
 ?>
 <div class="col-12 order-md-last mt-4 pb-5">
     <h4 class="d-flex justify-content-between align-items-center mb-3">
@@ -14,6 +13,7 @@ $total = 0;
     <?php if (count($orders)>0){?>
     <div class="list-group">
         <?php foreach($orders as $sale_order){
+            $total = 0;
             $order_items = $oim->get_orders_item($sale_order->id); ?>
             <a href="#<?php echo $sale_order->id?>" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" onclick="location.href='<?php echo ROOT_URL . 'user/?page=my_order&id=' . ($sale_order->id); ?>'">
                 <img src="../../front-end/assets/imgs/icons8lampada96.png" alt="logo" width="32" height="32" class="rounded-circle flex-shrink-0">
@@ -22,9 +22,10 @@ $total = 0;
                         <h6 class="mb-0">id Ordine: <?php echo $sale_order->id ?></h6>
                         <?php foreach($order_items as $item){ 
                             $product = $pm->get($item->product_id);
-                            $total += $product->price?>
+                            $total += $product->price * $item->quantity?>
                             <p class="mb-0 opacity-75"><strong>Prodotto: </strong><?php echo $product->name ?></p>
                             <p class="mb-0 opacity-75">Prezzo: <?php echo $product->price ?></p>
+                            <p class="mb-0 opacity-75">Quantità: <?php echo $item->quantity ?></p>
                             <hr></hr>
                         <?php }?>
                     </div>
